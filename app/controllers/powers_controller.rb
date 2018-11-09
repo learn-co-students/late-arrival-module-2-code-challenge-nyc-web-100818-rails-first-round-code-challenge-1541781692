fbd446fb41ca58d62b79cf80f38d3d22563edf90
sg3957 <sg3957@nyu.edu>
2018-11-09 12:54:22 -0500

    done

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,5 +1,27 @@
 class PowersController < ApplicationController
+
+  before_action :find_power, only: [:show]
+
   def index
     @powers = Power.all
+  end
+
+  def show
+    #code
+  end
+
+
+
+
+
+
+  private
+
+  def power_params
+    params.require(:power).permit(:name, :description)
+  end
+
+  def find_power
+    @power = Power.find_by(id: params[:id])
   end
 end
